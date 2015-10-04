a4d1221aa355c28062ff568526619cc3cae6a285
samgreenaway <sam@mohio.co.uk>
2015-10-04 10:25:29 +0100

    uploading project

---
+++ SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.SignalR;
+using SignalRChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test(string name, string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.addNewMessageToPage(name, message);
+            return View("Index");
+        }
+
+        [HttpPost]
+        public void Send(MessageModel model)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.addNewMessageToPage(model.name, model.msg);       
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRChat/app/controller.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+app.controller('chatController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.Messages = [];
+        
+        var chat = $.connection.chatHub;
+        
+        // Create a function that the hub can call back to display messages / Signalr client functions
+        chat.client.addNewMessageToPage = function (name, message) {
+            var obj = { name: name, message: message };
+            $scope.Messages.push(obj);
+            $scope.$apply(); // this is outside of angularjs, so need to apply
+        };
+
+        $scope.newMsg = {};
+        $scope.newMsg.name = 'Sam';
+        $scope.newMsg.msg = 'Test';
+
+        $scope.postAnswer = function () {
+            $http.post('Send', $scope.newMsg).
+                success(function (data) {
+                })
+        };
+        $.connection.hub.start();
+    }]);
